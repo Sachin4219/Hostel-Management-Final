2423f0ab4b1f0bb26204f4f3e7b04878694fd583
Sachin Shukla <91952010+Sachin4219@users.noreply.github.com>
2022-08-29 22:42:59 +0530

    Add files via upload

---
+++ hostel-management/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './About';
+import QuickLinks from './QuickLinks';
+import ContactUs from './ContactUs';
+
+function Footer() {
+    return (
+        <footer>
+            <About />
+            <QuickLinks />
+            <ContactUs />
+        </footer>
+    );
+}
+
+export default Footer;
